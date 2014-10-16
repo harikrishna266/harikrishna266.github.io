b9f6a68c5b04381c633a97090aeffcbefd5e7724
harikrishna266 <harikrishna266@gmail.com>
2014-10-17 01:01:25 +0530

    ui router

--- js/app.js
+++ js/app.js
@@ -1,29 +1,49 @@
-	var app = angular.module('myapp',['ngRoute']);
+	var app = angular.module('myapp',['ui.router']);
 
 
-	app.config(function($routeProvider){
-		$routeProvider
-			.when('/',{
+	app.config(function($stateProvider,$urlRouterProvider){
+		
+		$stateProvider
+			.state('home',{
+				url:'/',
 				controller:'homecontroller',
-				templateUrl:'partials/home.html'
+				templateUrl:'partials/home/home.html'
 			})
-			.when('/about',{
+			.state('home.about',{
+				url:'about',
 				controller:'aboutcontroller',
-				templateUrl:'partials/aboutus.html'
+				templateUrl:'partials/home/about.html'
 			})
-			.when('/contact',{
-				controller:'contactcontroller',
-				templateUrl:'partials/contact.html'
+			.state('about.facebook',{
+				url:'facebook',
+				controller:'aboutcontroller',
+				templateUrl:'partials/home/facebook.html'
 			})
-			.otherwise({
-				redirectTo:"/"
-			})
+
+	//	$urlRouterProvider.otherwise('/');	
+		// $routeProvider
+		// 	.when('/',{
+		// 		controller:'homecontroller',
+		// 		templateUrl:'partials/home.html'
+		// 	})
+		// 	.when('/about',{
+		// 		controller:'aboutcontroller',
+		// 		templateUrl:'partials/aboutus.html'
+		// 	})
+		// 	.when('/contact',{
+		// 		controller:'contactcontroller',
+		// 		templateUrl:'partials/contact.html'
+		// 	})
+		// 	.otherwise({
+		// 		redirectTo:"/"
+		// 	})
 	})
 
 
 
 	app.controller('homecontroller',function($scope){
-			$scope.text = "This is my home page";
+			
+			//$scope.text = "This is my home page";
 	})
 
 	app.controller('aboutcontroller',function($scope){
